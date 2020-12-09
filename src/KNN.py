07fcae01a82124e3a2eaf985d22e896611a8fb0e
Ines DOBOSZ <ines.dobosz@isen.yncrea.fr>
2020-12-09 17:26:10 +0100

    Svm & KNN & Perceptron & Gestion Donnees

---
+++ src/KNN.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+from numpy import *
+import gestion_donnees as gd
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from random import randint
+from random import shuffle
+import sys
+
+def warn(*args, **kwargs): pass
+import warnings
+warnings.warn = warn
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedShuffleSplit
+
+"""  
+
+################################
+# Execution en tant que script 
+#
+# tapper python KNN.py 1 0
+#
+# dans un terminal
+################################
+python KNN.py 1 0
+
+"""
+
+class KNN : 
+    etiquette_classe = 99
+    def __init__(self):
+        """
+        Algorithmes des K plus proches voisins, classification lineaire"""
+
+        self.nb_test = 0
+        self.nb_train = 0
+        self.test = 0
+
+        
+    
+    #Prédiction de la classe des k plus proches voisins selon
+    def predict_test(self, X_train, y_train, X_test, y_test, k): #k plus proches voisins
+        if k==0: k=1
+        print("Méthode k plus proches voisins, avec k = ", k)
+        distances = np.zeros(self.nb_train)
+        i=0
+        score=0
+        for imTest in X_test: #Pour tous les test 
+            index=0
+            resultat = np.zeros(self.etiquette_classe)
+            sorted_list = np.zeros(self.nb_train)
+            for imTrain in X_train: #pour tous les train de chaque test
+                diff = imTest-imTrain
+                distances[index] = np.linalg.norm(diff[0:63]) + np.linalg.norm(diff[64:127]) + np.linalg.norm(diff[128:191])
+                #distances[index] = np.linalg.norm(diff)
+                index+=1
+            distances=distances/3
+            sorted_list = np.argsort(distances) #trier par index des distances les plus petites
+            voisinsProches = sorted_list[:k] # k distances les plus petites
+            for j in range(0,k):
+                resultat[y_train[voisinsProches[j]]] += 1
+            result = np.argmax(resultat)
+            #print("Prédiction : ", result, ", étiquette : ", y_test[i])
+            if result==y_test[i]:
+                score+=1
+            i+=1
+        print("Réussite: {}%".format(score/i*100))
+
+    #Prédiction des k plus proches voisins en comparant les trois features (Margin, Shape, Texture)
+    def predictByFeaturesKN(self, X_train, y_train, X_test, k, y_test = []):
+
+        predictionsTot = np.zeros(len(X_test))
+        print("Méthode des k plus proches voisins appartenant à la même classe")
+        print("La classe prédite est celle qui correspond à la classe qu'on retrouve k fois, parmi les plus proches voisins")
+        distances = np.zeros(X_train.shape[0])
+
+        #Comparaison des distances entre les donnees de test et d'entrainement
+        i=0
+        score=0
+        for imTest in X_test:
+            index=0
+            resultat = np.zeros(self.etiquette_classe)
+            sorted_list = np.zeros(self.nb_train)
+            for imTrain in X_train:
+                diff = imTest-imTrain
+                # moyenne de chaque feature
+                distances[index] = np.linalg.norm(diff[0:63]) + np.linalg.norm(diff[64:127]) + np.linalg.norm(diff[128:191])
+                index+=1
+            distances=distances/3
+            sorted_list = np.argsort(distances) #trié par index des distances les plus petites
+            voisinsProches = sorted_list #  distance la plus petite
+            j=0
+
+            #Tant que l'on n'a pas k voisins appartenant à la même classe
+            while np.max(resultat)<k :
+                resultat[y_train[sorted_list[j]]] += 1
+                j+=1
+            result = np.argmax(resultat)
+            predictionsTot[i] = result
+
+            #Prédiction pour des donnees sansp cible (donnees de test)
+            if (y_test == []):
+                print("Prédiction : ", result)
+                i+=1
+
+            #Prediction pour des donnees avec cible
+            else :
+                if result==y_test[i]:
+                    score+=1
+                i+=1
+        #Précision totale
+        scoreTot = score/i*100
+        print("Réussite: {}%\n".format(score/i*100))
+                
+        return predictionsTot, scoreTot
+
+    #Affichage des groupes de points
+    #La couleur des points dépend de leur classe
+    #Chaque dimension correspond à la moyenne d'une feature
+    def affichage(self, X_train, y_train, test, k=1, y_test=[]):
+        colors = []
+        #On récupere les predictions
+        predictionTest,_ = self.predictByFeaturesKN(X_train, y_train, test, 1, y_test)
+        plt.figure(figsize=(30,30))
+        #Affichage des points en 3D
+        plt.axes(projection="3d")
+
+        #Pour toutes les étiquettes de classe, on assigne une couleur différente
+        for i in range(self.etiquette_classe):
+            colors.append('#%06X' % randint(0, 0xFFFFFF))
+
+        #tableau contenant donnees d'entrainement et donnees testees
+        points = np.zeros((self.nb_train+self.test,3))
+        i=0
+        #Ajout des points d'entrainement
+        for x in X_train:       
+            points[i] = self.moyenneFeatures(x)
+            c = colors[y_train[i]]      #différente couleur pour chaque etiquette de classe
+            plt.scatter(points[i, 0], points[i, 1], points[i, 2], c=c) 
+            i = i+1
+        i=0
+
+        #Ajout des donnees testees et ciblees sous forme de marker _
+        for t in test:
+            points[i+len(X_train)] = self.moyenneFeatures(t)
+            c = colors[int(predictionTest[i])]      #différente couleur pour chaque etiquette de classe
+            plt.scatter(points[i, 0], points[i, 1], points[i, 2], c=c, marker = '_')
+            i = i+1
+
+        plt.title('Margin Shape Texture')
+        plt.show()
+        
+        #points = np.zeros((self.nb_train+self.test,3))
+        #i=0
+        #plt.figure(figsize=(30,30))
+        #for x in X_train:
+        #    points[i] = self.moyenneFeatures(x)
+        #    c = colors[y_train[i]]
+        #    plt.scatter(points[i, 1], points[i, 2], c=c)
+        #    i = i+1
+        #i=0
+        #for t in test:
+        #    points[i+len(X_train)] = self.moyenneFeatures(t)
+        #    c = colors[int(predictionTest[i])]      #différente couleur pour chaque etiquette de classe
+        #    plt.scatter(points[i, 1], points[i, 2], c=c, marker = '_')
+        #    i = i+1
+        #plt.title('Shape Texture')
+        #plt.show()
+        
+        #plt.figure(figsize=(30,30))
+        #points = np.zeros((self.nb_train+self.test,3))
+        #i=0
+        #for x in X_train:
+        #    points[i] = self.moyenneFeatures(x)
+        #    c = colors[y_train[i]]
+        #    plt.scatter(points[i, 2], points[i, 0], c=c)
+        #    i = i+1
+        #i=0
+        #for t in test:
+        #    points[i+len(X_train)] = self.moyenneFeatures(t)
+        #    c = colors[int(predictionTest[i])]      #différente couleur pour chaque etiquette de classe
+        #    plt.scatter(points[i, 2], points[i, 0], c=c, marker = '_')
+        #    i = i+1
+        #plt.title('Texture Margin')
+        #plt.show()
+
+
+    #Calcul des moyennes de features pour une donnee
+    def moyenneFeatures(self, x):
+        moyMargin = np.linalg.norm(x[:64])
+        moyShape = np.linalg.norm(x[64:128])
+        moyTexture = np.linalg.norm(x[128:192])
+        data = np.array([moyMargin,moyShape,moyTexture])
+        return data
+
+    def validation_croisee(self, train, labels):
+        print(" --- Validation croisee --- ")
+        #Création de plusieurs divisons de l'ensemble des donnees
+        sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=23) #n_spilt divisions, 0.2 = test 0.8 = train
+        i=0
+        meanScoreCV = 0
+        for train_index, test_index in sss.split(train, labels): #pour chaque sous partie, on divise avec sss
+            i+=1
+            print("Itération ",i)
+            X_train, X_test = train.values[train_index], train.values[test_index]
+            y_train, y_test = labels[train_index], labels[test_index]
+            #Prediction pour chaque partie
+            _,score = self.predictByFeaturesKN(X_train, y_train, X_test, 1, y_test)
+            meanScoreCV += score
+        meanScoreCV = meanScoreCV / (i)
+        print("La validation croisée affiche une prédiction moyenne de %.2f \n" %(meanScoreCV))
+
+    #Prédiction d'un ensemble pour tous les k
+    def prediction_all_k(self, X_train, y_train, X_test, y_test):
+        print(" -- Prédiction pour tous les k ---")
+        result = np.zeros(8)
+        for k in range(1,9):
+            print("Prédiction pour k = ",k)
+            _,result[k-1] = self.predictByFeaturesKN(X_train, y_train, X_test, k, y_test)
+        plt.scatter(range(1,9),result)
+        plt.title('Margin Shape Texture')
+        plt.xlabel("Nombre de plus proches voisins")
+        plt.ylabel("Précision")
+        plt.show()
+
+
+
+
+def main():
+    
+     #Récupération des parametres 
+    if len(sys.argv) < 2:
+        usage = "\n k nombre de voisins pour la prédiction\
+        \n\t lin : 0: prédiction avec données de validation, 1: prédiction avec données de test"
+        print(usage)
+        return
+    k = sys.argv[1]
+
+    #Creation de l'objet
+    knn = KNN()
+    #Récupération des donnees
+    donnees = gd.GestionDonnees() 
+    [X_train, y_train, X_test, y_test, test, train, labels] = donnees.recupDonnees()#stockage données csv dans variables
+    
+    #Taille des échantillons
+    knn.nb_train = len(X_train)
+    knn.nb_test = len(X_test)
+    knn.test = len(test)
+
+    #Validation croisee
+    knn.validation_croisee(train, labels)
+
+    #Prédiction pour tous les k
+    knn.prediction_all_k(X_train, y_train, X_test, y_test)
+
+    
+    #Donnees de test, sans cible
+    if sys.argv[2] == 1:
+        knn.affichage(train, labels, test, k)
+
+    #Donnees de validation
+    else:
+        knn.affichage(X_train, y_train, X_test, k, y_test)
+
+
+if __name__ == "__main__":
+    main()
--- src/gestion_donnees.py
+++ src/gestion_donnees.py
@@ -1,59 +1,58 @@
-# -*- coding: utf-8 -*-
-
-#####
-# VotreNom (VotreMatricule) .~= À MODIFIER =~.
-###
 
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedShuffleSplit
 
+"""
+
+Changer self.train_add & self.test_add avec le chemin brut
+test
+
+"""
 
 class GestionDonnees:
-    def __init__(self, nb_train, nb_test, lineairement_sep):
-        self.nb_train = nb_train
-        self.nb_test = nb_test
-        self.lineairement_sep = lineairement_sep
-
-    def donnees_aleatoires(self, nb_data):
-        """
-        Fonction qui génère des données 2D aléatoires
-        nb_data : nb de donnees générées
-        """
-        if self.lineairement_sep:
-            nb_data_1_2 = int(nb_data / 2.0)
-        else:
-            nb_data_1_2 = int(nb_data / 3.0)
-            nb_data_2 = nb_data - int(2.0 * nb_data / 3.0)
-
-        x_1 = np.random.randn(nb_data_1_2, 2) + np.array([[5, 1]])  # Gaussienne centrée en mu_1_1=[5,1]
-        t_1 = np.ones(nb_data_1_2)
-        x_2 = np.random.randn(nb_data_1_2, 2) + np.array([[2, 3]])  # Gaussienne centrée en mu_2=[2,3]
-        t_2 = np.zeros(nb_data_1_2)
-        x = np.vstack([x_1, x_2])
-        t = np.hstack([t_1, t_2])
+    def __init__(self):
+        self.nb_train = 0
+        self.nb_test = 0
+        self.nb_split = 1
+        self.test_size = 0.2
+        self.train_add = r'C:\Users\inesd\source\repos\SessionProject\leaf-classification\train.csv'
+        self.test_add = r'C:\Users\inesd\source\repos\SessionProject\leaf-classification\test.csv'
 
 
-        if not self.lineairement_sep:
-            x_2 = np.random.randn(nb_data_2, 2) + np.array([[0, 4]])  # Gaussienne centrée en mu_1_1=[0,4]
-            t_2 = np.ones(x_2.shape[0])
+    def recupDonnees(self):
+        #Récupération des donnees d'entrainement et de test à partir des fichiers csv
+        train = pd.read_csv(self.train_add) #on récupère  les données d'entrainment (990)
+        test = pd.read_csv(self.test_add) #on récupère  les données d'entrainment (594)
+        train, labels, test, test_ids, classes = self.encode(train, test)
+        train.head(1)
 
-            # Fusionne toutes les données dans un seul ensemble
-            x = np.vstack([x, x_2])
-            t = np.hstack([t, t_2])
+        final_test = np.zeros((len(test),64*3))
+        sss = StratifiedShuffleSplit(n_splits=self.nb_split, test_size=self.test_size, random_state=1) #n_split divisions, 0.2 = test 0.8 = train
+        
+        #Valeur de test (sans étiquette de classe)
+        for i in range(0,len(test)):
+            final_test[i] = test.values[i]
 
-        # Mélange aléatoire des données
-        p = np.random.permutation(len(t))
-        x = x[p, :]
-        t = t[p]
+        i=0
+        #Mélange aléatoire
+        for train_index, test_index in sss.split(train, labels): #pour chaque sous partie, on divise avec sss
 
-        return x, t
-
-    def generer_donnees(self):
-        """
-        Fonction qui genere des donnees de test et d'entrainement.
-        nb_train : nb de donnees d'entrainement
-        nb_test : nb de donnees de test
-        """
-        x_train, t_train = self.donnees_aleatoires(self.nb_train)
-        x_test, t_test = self.donnees_aleatoires(self.nb_test)
-
-        return x_train, t_train, x_test, t_test
+            X_train, X_test = train.values[train_index], train.values[test_index]
+            y_train, y_test = labels[train_index], labels[test_index]
+        
+        self.nb_train = len(X_train)
+        self.nb_test = len(X_test)
+        self.test = len(final_test)
+        return (X_train, y_train, X_test, y_test, final_test, train, labels)
+    
+    def encode(self, train, test):
+        le = LabelEncoder().fit(train.species) 
+        labels = le.transform(train.species)           # encode les especes 
+        classes = list(le.classes_)                    # save column names for submission
+        test_ids = test.id                             # save test ids for submission
+        train = train.drop(['species', 'id'], axis=1)  
+        test = test.drop(['id'], axis=1)
+    
+        return train, labels, test, test_ids, classes
