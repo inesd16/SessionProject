d0642b26693f4b75570cc179964aa35685da0d95
Ines DOBOSZ <ines.dobosz@isen.yncrea.fr>
2020-12-10 13:45:23 +0100

    gitIgnore

---
+++ src/hello-world.py
@@ -0,0 +1,12 @@
+from numpy import *
+from sklearn.linear_model import Perceptron
+import matplotlib.pyplot as plt
+# read in some helpful libraries
+import nltk # the natural langauage toolkit, open-source NLP
+import pandas as pd # dataframes
+
+
+
+
+print("Goodbye, World!")
+print("Goodbye, World!")
